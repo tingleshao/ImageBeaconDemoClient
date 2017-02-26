e6d7c9f61df8e30be48755315d968c50d1405848
tingleshao <cshao@cs.unc.edu>
2017-02-25 23:02:39 -0500

    add String to MAT

--- app/src/main/java/com/example/chongshao/imagebeacondemoclient/MainActivity.java
+++ app/src/main/java/com/example/chongshao/imagebeacondemoclient/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,6 +19,12 @@ import java.io.InputStreamReader;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            // Handle initialization error
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = (ImageView)this.findViewById(R.id.imageView);
 
         imageView.setImageResource(R.drawable.images);
-        this.readFromCSV("foo");
+        String csvText = this.readFromCSV("foo");
+        Mat m = this.csvTextToMat(csvText, 64, 64);
     }
 
     public Mat imageFromDCTMat(Mat dctMat) {
@@ -32,19 +43,43 @@ public class MainActivity extends AppCompatActivity {
         return img;
     }
 
-    public void readFromCSV(String filename) {
+    public String readFromCSV(String filename) {
         InputStream inputStream = getApplicationContext().getResources().openRawResource(R.raw.android_icon);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line = null;
+        String text = "";
+
         try {
             line = reader.readLine();
             while (line != null) {
                 Log.d("DDL", line);
+                if (line != null) {
+                    text = text.concat(line);
+                    text = text.concat("\n");
+                }
                 line = reader.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return text;
     }
 
+    public Mat csvTextToMat(String csvText, int h, int w) {
+        Mat m = new Mat(64, 64, CvType.CV_64FC1);
+        // split the csvtext
+        String[] lines = csvText.split("\n");
+        Log.d("DDDL", Integer.toString(lines.length));
+        int row = 0;
+        for (String line : lines) {
+            String[] elements = line.split(",");
+            for (int i = 0; i < w; i++) {
+                m.put(row, i, Float.parseFloat(elements[i]));
+            }
+            row = row + 1;
+        }
+        Log.d("DDL:", m.dump());
+        Log.d("DDL:", Float.toString((float)m.get(63, 0)[0]));
+        return m;
+    }
 }
