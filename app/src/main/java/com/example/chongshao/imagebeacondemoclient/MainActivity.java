7ff19f366f8aa52ecbcb5b854b017f967d3fad5d
tingleshao <cshao@cs.unc.edu>
2017-02-25 22:30:51 -0500

    load iDCT, implement reading from CSV

--- app/src/main/java/com/example/chongshao/imagebeacondemoclient/MainActivity.java
+++ app/src/main/java/com/example/chongshao/imagebeacondemoclient/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.chongshao.imagebeacondemoclient;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
-// TODO: decode a sample JPEG image
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = (ImageView)this.findViewById(R.id.imageView);
 
         imageView.setImageResource(R.drawable.images);
+        this.readFromCSV("foo");
     }
+
+    public Mat imageFromDCTMat(Mat dctMat) {
+        Mat img = new Mat();
+        Core.idct(dctMat, img);
+        return img;
+    }
+
+    public void readFromCSV(String filename) {
+        InputStream inputStream = getApplicationContext().getResources().openRawResource(R.raw.android_icon);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = null;
+        try {
+            line = reader.readLine();
+            while (line != null) {
+                Log.d("DDL", line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
