debcb654124ecb22bd5c77ec43ffb15167e13765
tingleshao <cshao@cs.unc.edu>
2017-03-04 13:55:37 -0500

    add zlib decoding

--- app/src/main/java/com/example/chongshao/imagebeacondemoclient/MainActivity.java
+++ app/src/main/java/com/example/chongshao/imagebeacondemoclient/MainActivity.java
@@ -25,6 +25,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -53,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
     private boolean mIsScanning;
     private BluetoothAdapter mBTAdapter;
     private Button scanButton;
+    private Button testButton;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
 
@@ -111,6 +115,15 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
                 // TODO Auto-generated method stub
                 Log.d("DDL", "scan button clicked");
                 startScan();
+            }
+        });
+
+        testButton = (Button)this.findViewById(R.id.testButton);
+        testButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("DDL", "test button clicked");
+                decodeByteArray();
             }
         });
 
@@ -219,7 +232,9 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
 //               mDeviceAdapter.update(device, rssi, scanRecord);
 //            }
 //        });
-        Log.d("Ddl:", ByteArrayToString(scanRecord) + Integer.toString(rssi));
+        if (rssi > -60) {
+            Log.d("Ddl:", ByteArrayToString(scanRecord) + Integer.toString(rssi));
+        }
     }
 
 
@@ -237,7 +252,6 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
 
          return msg;
     }
-
 
     // permission
     @Override
@@ -261,5 +275,49 @@ public class MainActivity extends AppCompatActivity implements BluetoothAdapter.
                 return;
             }
         }
+    }
+
+    // decoding
+    public void decodeByteArray() {
+        try {
+            // Encode a String into bytes
+            String inputString = "x\\x9c\\xed\\x93\\xd9N\\xc30\\x10Eg\\xb1\\x93\\xb6,\\x12\\x9b\\xe0K\\xf8\\xff\\xbf\\xe1\\t\\xa9T\\x14R/3\\x8c\\xdbJ-\\xf0P;E\\xe2%G\\x91\\x95\\x87\\\\\\xdf;K\\x9e_\\xc2\\xe3\\xc3Z\\x16i\\xbd|}\\x8fC\\x02b\\xd7\\xb9\\xf2\\xa0\\xa4(p\\x8a\\x9b\\xbb\\xb7\\x18{\\x8e\\x17\\xb4`\\x10 EH\\n\\xc8D\\x08tRm\\xf4\\xab\\xfb\\xd5\\x13$G\\x91\\xbb`'\\xa0 \\x05\\x91p\\xda\\xbb\\x90\\xd2\\xed\\x8a\\xe7\\xcb\\x19\\x0c\\x0e\\xc8\\x9b\\xc6lQA\\x19\\xd8\\xd5\\xe8\\xa3\\xf7\\x1eXis\\ry\\x16\\xbado\\xbd\\x00\\xe7\\x0cu\\xfe \\x97\\xf8\\x91gWC\\xf7\\xe93\\x11\\xd8\\x01\\xd6\\x03!\\xad\\x93\\xf7\\xb1K\\x19\\xe6\\x83\\xcb\\xda\\rV:PR\\xc9u\\xda\\xc2,d\\xaf\\xec6\\x82V?G\\x04u\\xd6\\xc1\\xba\\xd6\\x17JV\\x96$\\x9d8\\xd9\\xf8\\xcc\\xd1\\x82S\\x83?fs\\x0c\\xd6\\xf3\\x80\\xc9\\x81\\x82U`\\xe3\\xaf\\xd7\\x8b\\x99q`\\xce\\x89\\xadq\\xb9\\\\\\xa1\\rr\\xc8\\xc8\\xd1F\\xa5e\\xe6\\x9a<\\xa4\\xe2\\xdf\\x84}NVE\\xd1\\x91\\n`\\xd5\\xda\\x1c\\xc9\\x85M*d\\x95\\x97VbKx\\xc3o\\xd7L\\x95\\xf7\\t*\\xb7\\xee`O\\xdbn\\xe3\\xee\\x92F\\xf3\\xed\\x05(\\x16\\xdef\\x86X\\xbd\\xf3G\\x14G\\x82\\xed\\xb6g?\\xc2\\x1e!\\xef\\xc27\\xcfmGi\\xd9\\x08\\xdb\\xefW\\xec\\x93\\x8cad\\xec\\x83\\xfe<\\xb9\\xf5\\rGL\\xedH\\xdf\\xf4\\xb7\\xfd\\xc6\\xd4\\xad+\\xff3\\xc1\\x99\\xea3\\xa7\\xff7\\xcb\\xf3_\\xf6\\x13\\x13\\x13\\x13\\x13\\x13\\x13\\x13\\xcd|\\x01+8\\xa8a";
+            byte[] input = inputString.getBytes("US-ASCII");
+       //     ByteBuffer wrapped = ByteBuffer.wrap(input[i]); // big-endian by default
+       //     short num = wrapped.getShort(); // 1
+
+            for (int i = 0; i < input.length; i++) {
+
+             //   ByteBuffer wrapped = ByteBuffer.wrap(input[i]); // big-endian by default
+             //   short num = wrapped.getShort(); // 1
+                Log.d("ddl", String.valueOf((int)input[i]));
+            }
+            // Compress the bytes
+       //     byte[] output = new byte[100];
+       //     Deflater compresser = new Deflater();
+       //     compresser.setInput(input);
+       //     compresser.finish();
+       //     int compressedDataLength = compresser.deflate(output);
+       //     compresser.end();
+
+            // Decompress the bytes
+            Inflater decompresser = new Inflater();
+            decompresser.setInput(input, 0, 388);
+            byte[] result = new byte[4096];
+            int resultLength = decompresser.inflate(result);
+            decompresser.end();
+
+            // Decode the bytes into a String
+        //    String outputString = new String(result, 0, resultLength, "UTF-8");
+            Log.d("DDL", result.toString());
+        } catch(java.io.UnsupportedEncodingException ex) {
+            Log.d("DDL", "Some problem1");
+
+        } catch (java.util.zip.DataFormatException ex) {
+            // handle
+            Log.d("DDL", "Some problem2");
+
+        }
+
     }
 }
